cba4134582491ed36d46c7faa0d4fbe6d3115681
Filipe Batista <filipe.x@live.com>
2021-03-25 06:24:20 -0300

    limpando repositorio

--- admin/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //baseURL: 'http://localhost:3333',
-    baseURL: 'https://agenciaperform.com.br',
-});
-
-export default api;
--- scripts/skip.js
+++
@@ -1,9 +0,0 @@
-
-process.exit(0);
-
-// if (process.env.SKIP_BUILD) {
-//     process.exit(0);
-// } else {
-//     process.exit(1);
-// }
-//test
